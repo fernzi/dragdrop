0f3a900b5d1787e73ee935fe0a491a5fa61db763
Fern Zapata <fern@zapata.cc>
2023-11-22 22:07:50 -0700

    Chore: Add local variables for Emacs
    …in preparation for Guix.

---
+++ .dir-locals.el
@@ -0,0 +1,16 @@
+((nil
+ . ((fill-column . 70)
+    (tab-width . 4)))
+ (c++-mode
+  . ((c-file-style . "linux")
+     (c-basic-offset . 4)
+     (indent-tabs-mode . t)))
+ (c++-ts-mode
+  . ((c-ts-mode-indent-offset . 4)
+     (indent-tabs-mode . t)))
+ (emacs-lisp-mode
+  . ((indent-tabs-mode . nil)))
+ (lisp-data-mode
+  . ((indent-tabs-mode . nil)))
+ (scheme-mode
+  . ((indent-tabs-mode . nil))))
